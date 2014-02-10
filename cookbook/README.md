cd84350813633e9d19b49e69feaaab2a03178f13
Chris Bednarski <banzaimonkey@gmail.com>
2014-02-09 19:16:24 -0800

    Added rouket cookbook

---
+++ cookbook/README.md
@@ -0,0 +1,13 @@
+# rouket cookbook
+
+# Requirements
+
+# Usage
+
+# Attributes
+
+# Recipes
+
+# Author
+
+Author:: YOUR_NAME (<YOUR_EMAIL>)
---
+++ cookbook/recipes/default.rb
@@ -0,0 +1,11 @@
+#
+# Cookbook Name:: rouket
+# Recipe:: default
+#
+# Copyright (C) 2014 YOUR_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+
+include_recipe "semitas::nginx"
+
